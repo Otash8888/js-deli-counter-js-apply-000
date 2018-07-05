9f3584056dcd8d3a6831aa61733e1a77dcee42c2
Otabek Kamalov <otakam86@gmail.com>
2018-07-05 16:50:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+var n=0;
+function takeANumber(katzDeli) {
+   
+  
+  katzDeli.push(n + katzDeli.length);
+  return `Welcome, You are number ${n} in line.`
+
+  
+}
+function nowServing(katzDeli) {
+if (katzDeli.length === 0) {
+  return "There is nobody waiting to be served!"
+} else{
+ var str = `Currently serving ${n}.`
+ katzDeli.shift();
+ return str
+}
+}
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+  return "The line is currently empty."
+} else{
+ var newArray=[];
+ for (let i=0;i<katzDeli.length;i++){
+   newArray[i]=` ${i+1}. ${katzDeli[i]}`
+ }
+ 
+return `The line is currently:${newArray.toString()}`
+}
+  
+}
